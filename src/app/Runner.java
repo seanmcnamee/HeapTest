b671620dc2cee2c3697d73b4ff740ce16888234d
Sean <seanmcnamee.45@gmail.com>
2019-12-23 12:53:02 -0500

    working

--- src/app/Runner.java
+++ src/app/Runner.java
@@ -7,17 +7,41 @@ public class Runner {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello Java");
         BST heap = new BST();
+        Simple s4 = new Simple(0, 4);
 
         for (int i = 0; i <= 10; i++) {
-            heap.addIterative(new BTNode(i));
+            int rndNum = (int)(Math.random()*100);
+            if (i==4) {
+                heap.add(s4);
+            }   else {
+                heap.add(new Simple(0, i));
+            }
         }
 
-        System.out.println("Root: " + heap.getRoot().getData());
+        //System.out.println("Root: " + heap.getRoot().getPriority());
         
-        heap.preOrder(heap.getRoot());
-        System.out.println();
-        heap.inOrder(heap.getRoot());
-        System.out.println();
-        heap.postOrder(heap.getRoot());
+        //heap.preOrder(heap.getRoot());
+        //System.out.println();
+        //heap.inOrder(heap.getRoot());
+        //System.out.println();
+        //heap.postOrder(heap.getRoot());
+        
+        
+        System.out.println("DeQueue.....\n\n");
+        BST tempHeap = new BST();
+        while (heap.getRoot()!=null) {
+            Simple temp = (Simple)heap.deQueue();
+            temp.render();
+            tempHeap.add(temp.resetConnections());
+        }
+
+        tempHeap.updatePriority(s4.setPriority(22));
+
+        
+        while (tempHeap.getRoot()!=null) {
+            Simple temp = (Simple)tempHeap.deQueue();
+            temp.render();
+        }
+
     }
 }
---
+++ src/app/Simple.java
@@ -0,0 +1,32 @@
+package app;
+
+/**
+ * Simple
+ */
+public class Simple extends Drawable{
+
+    private int x, y;
+
+    public Simple(int x, int y){
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+    public void render() {
+        System.out.println(y);
+    }
+
+    @Override
+    public int getPriority() {
+        return this.y;
+    }
+
+    @Override
+    public Simple setPriority(int... y) {
+        System.out.println("Setting priority");
+        this.y = y[0];
+        return this;
+    }
+
+}
